2bcf4ff27e8184807cd7f7d1874e24ab2251209c
Nageswar <nagesh23479@gmail.com>
2026-02-07 10:30:28 +0530

    disk usage

--- 10-disk-usage.sh
+++ 10-disk-usage.sh
@@ -5,21 +5,20 @@
     - name: Run df -h
       shell: df -hT | grep -i tmpfs || true
       register: tmpfs_usage
-
     - name: Show disk usage
       debug: 
         # msg: "{{ inventory_hostname }}:\n{{ tmpfs_usage.stdout | default('No tmpfs mounts found') }}"
         var: tmpfs_usage.stdout_lines
   tasks:
-    - name: Check if nginx is installed
-      command: nginx -v
-      register: nginx_check
-      ignore_errors: yes
+    # - name: Check if nginx is installed
+    #   command: nginx -v
+    #   register: nginx_check
+    #   ignore_errors: yes
     - name: Install nginx if not present
       yum:
         name: nginx
         state: present
-      when: nginx_check.rc != 0
+    #   when: nginx_check.rc != 0
       tags: install
     - name: Ensure nginx service is running
       service:
